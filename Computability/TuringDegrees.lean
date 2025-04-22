72d9d2dffa480b1df7dd606f36293610a0355651
tannerduve <tannerduve@gmail.com>
2025-04-22 03:24:16 -0400

    Generalized Oracles

--- Computability/TuringDegrees.lean
+++ Computability/TuringDegrees.lean
@@ -1,188 +1,21 @@
-import Computability.Encoding
-import Mathlib.Order.Lattice
+import Computability.TuringReductions
+import Mathlib.GroupTheory.Perm.Basic
+
+/--
+Turing degrees are the equivalence classes of partial functions under Turing equivalence.
+-/
+abbrev TuringDegree :=
+  Antisymmetrization _ TuringReducible
+
+instance TuringDegree.isPartialOrder : PartialOrder TuringDegree :=
+  @instPartialOrderAntisymmetrization (ℕ →. ℕ)
+    {le := TuringReducible, le_refl := TuringReducible.refl, le_trans := @TuringReducible.trans}
 
 /-
-This section defines the Turing degrees and proves that they form an upper semilattice.
+Define the automorphism group of the Turing degrees.
 -/
 
--- Stuff about quotients is hidden in a namespace
-namespace Hidden
-universe u v
-
-axiom Quot : {α : Sort u} → (α → α → Prop) → Sort u
-
-axiom Quot.mk : {α : Sort u} → (r : α → α → Prop) → α → Quot r
-
-axiom Quot.ind :
-    ∀ {α : Sort u} {r : α → α → Prop} {β : Quot r → Prop},
-      (∀ a, β (Quot.mk r a)) → (q : Quot r) → β q
-
-axiom Quot.lift :
-    {α : Sort u} → {r : α → α → Prop} → {β : Sort u} → (f : α → β)
-    → (∀ a b, r a b → f a = f b) → Quot r → β
-
-axiom Quot.sound :
-      ∀ {α : Type u} {r : α → α → Prop} {a b : α},
-        r a b → Quot.mk r a = Quot.mk r b
-end Hidden
-
--- We can now define a setoid on functions from ℕ to ℕ under the turing equivalence relation
-instance TD : Setoid (ℕ →. ℕ) where
-  r := turing_equivalent
-  iseqv := ⟨turing_equiv_refl, @turing_equiv_symmetric, @turing_equiv_trans⟩
-
--- Define the Turing degrees as the set of equivalence classes under turing equivalence
-def TuringDegree := Quot turing_equivalent
-
--- Define the join operation on partial functions:
-def join (f g : ℕ →. ℕ) : ℕ →. ℕ :=
-  λ n =>
-    if n % 2 = 0 then
-      (f (n / 2)).map (λ x => 2 * x)
-    else
-      (g (n / 2)).map (λ y => 2 * y + 1)
-
-infix:99 "⊔" => join
-
-lemma reduce_join {f g : ℕ →. ℕ} :
-  f ≤ᵀ (f ⊔ g) ∧ g ≤ᵀ (f ⊔ g) := sorry
-
-lemma reduce_join₁ {f g h : ℕ →. ℕ} :
-  f ≤ᵀ h → g ≤ᵀ h → f ⊔ g ≤ᵀ h := sorry
-
-/-
-To do - We want to show Turing degrees form an upper semilattice.
-
-To do this we need to lift our join operation and the turing reducibility
-relation to Turing degrees. This requires an explicit proof that
-the operations and relations respect the equivalence, ie:
-
-∀ f g f' g', f ≡ᵀ f' → g ≡ᵀ g' → f ⊕ g ≡ᵀ f' ⊕ g'
-∀ f g f' g', f ≡ᵀ f' → g ≡ᵀ g' → f ≤ᵀ g → f' ≤ᵀ g'
-
-Once we've done this, we prove that the lifted operations and relations
-form a semilattice, ie. a lattice with a bottom element and a join operation.
--/
-
-#check Quot.lift
-
-theorem reduce_lifts : ∀ (a b : ℕ →. ℕ), a ≡ᵀ b → (f ≤ᵀ a ↔ f ≤ᵀ b) := by
-  intros a b aEqb
-  constructor
-  intro fReda
-  unfold turing_equivalent at *
-  cases' aEqb with lt rt
-  apply turing_reduce_trans fReda lt
-  intro fRedb
-  unfold turing_equivalent at *
-  cases' aEqb with lt rt
-  apply turing_reduce_trans fRedb rt
-
-theorem reduce_lifts₁ : ∀ (a b₁ b₂ : ℕ →. ℕ), b₁≡ᵀb₂ → (a≤ᵀb₁) = (a≤ᵀb₂) := by
-  intros a b₁ b₂ bEqb
-  apply propext
-  constructor
-  intro aRedb₁
-  unfold turing_equivalent at *
-  apply turing_reduce_trans aRedb₁ bEqb.1
-  intro aRedb₂
-  unfold turing_equivalent at *
-  apply turing_reduce_trans aRedb₂ bEqb.2
-
-theorem reduce_lifts₂ : ∀ (f g h : ℕ →. ℕ), f ≡ᵀ g → (turing_reducible f h = turing_reducible g h) := by
-  intros f g h fEqg
-  apply propext
-  constructor
-  intro fRedh
-  unfold turing_equivalent at *
-  apply turing_reduce_trans fEqg.2 fRedh
-  intro gRedh
-  unfold turing_equivalent at *
-  apply turing_reduce_trans fEqg.1 gRedh
-
--- Lift the turing reducibility relation to Turing degrees via quotient construction
-def TuringDegree.turing_red (d₁ d₂ : TuringDegree) : Prop :=
-  @Quot.lift₂ _ _ Prop (turing_equivalent) (turing_equivalent) (turing_reducible) (reduce_lifts₁) (reduce_lifts₂) d₁ d₂
-
-theorem join_lifts₁ : ∀ (a b₁ b₂ : (ℕ →. ℕ)), turing_equivalent b₁ b₂ → (Quot.mk turing_equivalent (join a b₁)) = (Quot.mk turing_equivalent (join a b₂)) := by
-  sorry
-
-theorem join_lifts₂ : ∀ (a₁ a₂ b : (ℕ →. ℕ)), turing_equivalent a₁ a₂ → (Quot.mk turing_equivalent (join a₁ b)) = (Quot.mk turing_equivalent (join a₂ b)) := by
-  sorry
-
--- Lift the join operation to Turing degrees via quotient construction
-def TuringDegree.join (d₁ d₂ : TuringDegree) : TuringDegree := by
-  apply Quot.lift₂ _ _ _ d₁ d₂
-  intros f g
-  apply Quot.mk turing_equivalent (f ⊔ g)
-  intros f g h gEqh
-  apply join_lifts₁ f g h gEqh
-  intros f g h fEqg
-  apply join_lifts₂ f g h fEqg
-
-def jump (f : ℕ →. ℕ) : ℕ →. ℕ :=
-λ e => (φ f e) e
-
-#check Quot.lift
-
-theorem jump_lifts : ∀ (a b : ℕ →. ℕ), a ≡ᵀ b → (Quot.mk turing_equivalent (jump a)) = (Quot.mk turing_equivalent (jump b)) := by
-  sorry
-
--- Lift the jump operator to Turing degrees via quotient construction
-def TuringDegree.jump (d : TuringDegree) : TuringDegree :=
-  @Quot.lift _ _ _ _ (jump_lifts) d
-
-def degree_zero : TuringDegree := Quot.mk turing_equivalent (λ _ => 0)
-
-def low (f : ℕ →. ℕ) : Prop := (jump f) ≡ᵀ (jump (λ _ => 0))
-
--- Prove that Turing Degrees forms an upper semilattice
-instance : SemilatticeSup TuringDegree where
-  sup := TuringDegree.join
-  le := TuringDegree.turing_red
-  le_refl := by
-    apply Quot.ind
-    intro a
-    apply turing_reduce_refl
-  le_trans := by
-    apply Quot.ind
-    intro a
-    apply Quot.ind
-    intro b
-    apply Quot.ind
-    intro c
-    exact turing_reduce_trans
-  le_antisymm := by
-    apply Quot.ind
-    intro a
-    apply Quot.ind
-    intro b
-    intros aRedb bReda
-    apply Quot.sound
-    have aRedb' : a ≤ᵀ b := aRedb
-    have bReda' : b ≤ᵀ a := bReda
-    unfold turing_equivalent at *
-    constructor
-    assumption
-    assumption
-  le_sup_left := by
-    apply Quot.ind
-    intro a
-    apply Quot.ind
-    intro b
-    exact reduce_join.1
-  le_sup_right := by
-    apply Quot.ind
-    intro a
-    apply Quot.ind
-    intro b
-    exact reduce_join.2
-  sup_le := by
-    apply Quot.ind
-    intro a
-    apply Quot.ind
-    intro b
-    apply Quot.ind
-    intro c
-    intro aRedc bRedc
-    exact reduce_join₁ aRedc bRedc
+-- Define join of Turing Degrees
+-- Define an automorphism as a bijection `f : TuringDegrees → TuringDegrees`
+-- preserving and reflecting order and join
+-- State and prove skeleton of countability argument
--- Computability/TuringReductions.lean
+++ Computability/TuringReductions.lean
@@ -1,107 +1,210 @@
+/-
+Copyright (c) 2025 Tanner Duve. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Tanner Duve, Elan Roth
+-/
+
 import Mathlib.Computability.Primrec
 import Mathlib.Computability.Partrec
-import Mathlib.Computability.Reduce
 import Mathlib.Data.Part
-import Mathlib.Tactic.Cases
-import Mathlib.Tactic.Lemma
+import Mathlib.Order.Antisymmetrization
 
-/-
-This section defines a model of oracle computability and defines Turing reducibility/Turing equivalence and proves that it is an equivalence relation.
--/
+/-!
+# Oracle Computability and Turing Degrees
 
-/-
-The type of partial functions recursive in an oracle g is the smallest type containing
-the constant zero, the successor, left and right projections, the oracle g, and is closed under
+This file defines a model of oracle computability, introduces Turing reducibility and equivalence,
+proves that Turing equivalence is an equivalence relation, and defines Turing degrees as the
+quotient under this relation.
+
+## Main Definitions
+- `RecursiveIn O f`:
+  An inductive definition representing that a partial function `f` is partial recursive given access
+  to a set of oracles O.
+- `TuringReducible`: A relation defining Turing reducibility between partial functions.
+- `TuringEquivalent`: An equivalence relation defining Turing equivalence between partial functions.
+- `TuringDegree`: The type of Turing degrees, defined as the quotient of partial functions under
+  `TuringEquivalent`.
+
+## Notation
+- `f ≤ᵀ g` : `f` is Turing reducible to `g`.
+- `f ≡ᵀ g` : `f` is Turing equivalent to `g`.
+
+## Implementation Notes
+
+The type of partial functions recursive in an oracle `g` is the smallest type containing
+the constant zero, the successor, projections, the oracle `g`, and is closed under
 pairing, composition, primitive recursion, and μ-recursion.
+
+## References
+* [Carneiro2018] Carneiro, Mario.
+  *Formalizing Computability Theory via Partial Recursive Functions*.
+  arXiv preprint arXiv:1810.08380, 2018.
+* [Odifreddi1989] Odifreddi, Piergiorgio.
+  *Classical Recursion Theory: The Theory of Functions and Sets of Natural Numbers,
+  Vol. I*. Springer-Verlag, 1989.
+* [Soare1987] Soare, Robert I. *Recursively Enumerable Sets and Degrees*. Springer-Verlag, 1987.
+* [Gu2015] Gu, Yi-Zhi. *Turing Degrees*. Institute for Advanced Study, 2015.
+
+## Tags
+Computability, Oracle, Turing Degrees, Reducibility, Equivalence Relation
 -/
-inductive RecursiveIn (g : ℕ →. ℕ) : (ℕ →. ℕ) → Prop
-  | zero : RecursiveIn g (λ _ => 0)
-  | succ : RecursiveIn g Nat.succ
-  | left : RecursiveIn g (λ n => (Nat.unpair n).1)
-  | right : RecursiveIn g (λ n => (Nat.unpair n).2)
-  | oracle : RecursiveIn g g
-  | pair {f h : ℕ →. ℕ} (hf : RecursiveIn g f) (hh : RecursiveIn g h) :
-      RecursiveIn g (λ n => (Nat.pair <$> f n <*> h n))
-  | comp {f h : ℕ →. ℕ} (hf : RecursiveIn g f) (hh : RecursiveIn g h) :
-      RecursiveIn g (λ n => h n >>= f)
-  | prec {f h : ℕ →. ℕ} (hf : RecursiveIn g f) (hh : RecursiveIn g h) :
-      RecursiveIn g (λ p =>
+open Primrec Nat.Partrec Part
+
+/-
+This section defines a model of oracle computability and defines
+Turing reducibility and Turing equivalence. We define the Turing Degrees as the quotient under
+Turing equivalence relation
+-/
+
+/--
+The type of partial functions recursive in a set of oracle `O` is the smallest type containing
+the constant zero, the successor, left and right projections, each oracle `g ∈ O`,
+and is closed under pairing, composition, primitive recursion, and μ-recursion.
+-/
+inductive RecursiveIn (O : Set (ℕ →. ℕ)) : (ℕ →. ℕ) → Prop
+  | zero : RecursiveIn O (fun _ => 0)
+  | succ : RecursiveIn O Nat.succ
+  | left : RecursiveIn O (fun n => (Nat.unpair n).1)
+  | right : RecursiveIn O (fun n => (Nat.unpair n).2)
+  | oracle : ∀ g ∈ O, RecursiveIn O g
+  | pair {f h : ℕ →. ℕ} (hf : RecursiveIn O f) (hh : RecursiveIn O h) :
+      RecursiveIn O (fun n => (Nat.pair <$> f n <*> h n))
+  | comp {f h : ℕ →. ℕ} (hf : RecursiveIn O f) (hh : RecursiveIn O h) :
+      RecursiveIn O (fun n => h n >>= f)
+  | prec {f h : ℕ →. ℕ} (hf : RecursiveIn O f) (hh : RecursiveIn O h) :
+      RecursiveIn O (fun p =>
         let (a, n) := Nat.unpair p
-        n.rec (f a) (λ y IH => do
+        n.rec (f a) (fun y IH => do
           let i ← IH
           h (Nat.pair a (Nat.pair y i))
         )
       )
-  | rfind {f : ℕ →. ℕ} (hf : RecursiveIn g f) :
-      RecursiveIn g (λ a =>
-        Nat.rfind (λ n => (λ m => m = 0) <$> f (Nat.pair a n))
+  | rfind {f : ℕ →. ℕ} (hf : RecursiveIn O f) :
+      RecursiveIn O (fun a =>
+        Nat.rfind (fun n => (fun m => m = 0) <$> f (Nat.pair a n))
       )
-
-def Partrec₃ (f : ℕ →. ℕ) : Prop :=
-  RecursiveIn (λ _ => 0) f
-
-/-
-f is turing reducible to g if f is recursive in g
+/--
+`f` is turing reducible to `g` if `f` is partial recursive given access to the oracle `g`
 -/
-def turing_reducible (f g : ℕ →. ℕ) : Prop :=
-  RecursiveIn g f
+abbrev TuringReducible (f g : ℕ →. ℕ) : Prop :=
+  RecursiveIn {g} f
 
-infix:50 "≤ᵀ" => turing_reducible
+@[inherit_doc TuringReducible]
+infix:50 "≤ᵀ" => TuringReducible
 
-def turing_equivalent (f g : ℕ →. ℕ) : Prop :=
-  f ≤ᵀ g ∧ g ≤ᵀ f
+/--
+`f` is Turing equivalent to `g` if `f` is reducible to `g` and `g` is reducible to `f`.
+-/
+abbrev TuringEquivalent (f g : ℕ →. ℕ) : Prop :=
+  AntisymmRel TuringReducible f g
 
-infix:50 "≡ᵀ" => turing_equivalent
+@[inherit_doc TuringEquivalent]
+infix:50 "≡ᵀ" => TuringEquivalent
 
--- Proof that turing_reducible is reflexive
-theorem turing_reduce_refl (f : ℕ →. ℕ) : f ≤ᵀ f := RecursiveIn.oracle
+/--
+If a function is partial recursive, then it is recursive in every partial function.
+-/
+lemma Nat.Partrec.turingReducible (f : ℕ →. ℕ) (pF : Nat.Partrec f) (g : ℕ →. ℕ) : f ≤ᵀ g := by
+  induction' pF with f' g' _ _ ih₁ ih₂ f' g' _ _ ih₁ ih₂ f' g' _ _ ih₁ ih₂ f' _ ih
+  repeat {constructor}
+  · case pair =>
+    apply RecursiveIn.pair ih₁ ih₂
+  · case comp =>
+    apply RecursiveIn.comp ih₁ ih₂
+  · case prec =>
+    apply RecursiveIn.prec ih₁ ih₂
+  · case rfind =>
+    apply RecursiveIn.rfind ih
 
-theorem turing_red_refl : Reflexive turing_reducible := λ f => turing_reduce_refl f
+/--
+If a function is recursive in the constant zero function,
+then it is partial recursive.
+-/
+lemma TuringReducible.partrec_of_zero (f : ℕ →. ℕ) (fRecInZero : f ≤ᵀ fun _ => Part.some 0) :
+  Nat.Partrec f := by
+  induction' fRecInZero with g hg g h _ _ ih₁ ih₂ g h _ _ ih₁ ih₂ g h _ _ ih₁ ih₂ g _ ih
+  repeat {constructor}
+  · rw [Set.mem_singleton_iff] at hg; rw [hg];
+    exact Nat.Partrec.zero
+  repeat {constructor; assumption; try assumption}
 
--- Proof that turing_equvalent is reflexive
-theorem turing_equiv_refl : Reflexive turing_equivalent := λ f => ⟨turing_reduce_refl f, turing_reduce_refl f⟩
+/--
+A partial function `f` is partial recursive if and only if it is recursive in
+every partial function `g`.
+-/
+theorem partrec_iff_partrec_in_everything (f : ℕ →. ℕ) : Nat.Partrec f ↔ ∀ g, f ≤ᵀ g :=
+  ⟨(·.turingReducible), (· _ |>.partrec_of_zero)⟩
 
--- Proof that turing_reducible is symmetric
-theorem turing_equiv_symm {f g : ℕ →. ℕ} (h : f ≡ᵀ g) : g ≡ᵀ f :=
-  ⟨h.2, h.1⟩
+theorem TuringReducible.refl (f : ℕ →. ℕ) : f ≤ᵀ f := by
+  apply RecursiveIn.oracle; simp
 
--- Proof that turing_equivalent is symmetric
-theorem turing_equiv_symmetric : Symmetric turing_equivalent := λ _ _ => turing_equiv_symm
+instance : IsRefl (ℕ →. ℕ) TuringReducible :=
+  ⟨fun f => TuringReducible.refl f⟩
 
--- Proof that turing_reducible is transitive
-theorem turing_reduce_trans {f g h : ℕ →. ℕ} :
-  f ≤ᵀ g → g ≤ᵀ h → f ≤ᵀ h := by
-  intro hg hh
-  unfold turing_reducible at *
-  induction hg
-  · apply RecursiveIn.zero
-  · apply RecursiveIn.succ
-  · apply RecursiveIn.left
-  · apply RecursiveIn.right
-  · apply hh
-  case pair f' h' _ _ hf_ih hh_ih =>
-    apply RecursiveIn.pair
-    · apply hf_ih
-    · apply hh_ih
-  case comp f' h' _ _ hf_ih hh_ih =>
-    apply RecursiveIn.comp
-    · apply hf_ih
-    · apply hh_ih
-  case prec f' h' _ _ hf_ih hh_ih =>
-    apply RecursiveIn.prec
-    · apply hf_ih
-    · apply hh_ih
-  case rfind f' _ hf_ih =>
-    apply RecursiveIn.rfind
-    · apply hf_ih
+theorem TuringReducible.trans {f g h : ℕ →. ℕ} (hg : f ≤ᵀ g) (hh : g ≤ᵀ h) :
+    f ≤ᵀ h := by
+  induction' hg with g' hg g' h' _ _ ih₁ ih₂ g' h' _ _ ih₁ ih₂ g' h' _ _ ih₁ ih₂ g' _ ih
+  repeat {constructor}
+  · rw [Set.mem_singleton_iff] at hg; rw [hg]; exact hh
+  · case pair =>
+    apply RecursiveIn.pair ih₁ ih₂
+  · case comp =>
+    apply RecursiveIn.comp ih₁ ih₂
+  · case prec =>
+    apply RecursiveIn.prec ih₁ ih₂
+  · case rfind =>
+    apply RecursiveIn.rfind ih
 
+instance : IsTrans (ℕ →. ℕ) TuringReducible :=
+  ⟨@TuringReducible.trans⟩
 
--- Proof that turing_equivalent is transitive
-theorem turing_equiv_trans : Transitive turing_equivalent :=
-λ _ _ _ ⟨fg₁, fg₂⟩ ⟨gh₁, gh₂⟩ => ⟨turing_reduce_trans fg₁ gh₁, turing_reduce_trans gh₂ fg₂⟩
+instance : IsPreorder (ℕ →. ℕ) TuringReducible where
+  refl := TuringReducible.refl
 
--- Equivalence relation instance for turing_equivalent
-instance : Equivalence (turing_equivalent) where
-  refl := turing_equiv_refl
-  symm := turing_equiv_symm
-  trans := @turing_equiv_trans
+theorem TuringEquivalent.equivalence : Equivalence TuringEquivalent :=
+  (AntisymmRel.setoid _ _).iseqv
+
+@[refl]
+theorem TuringEquivalent.refl (f : ℕ →. ℕ) : f ≡ᵀ f :=
+  Equivalence.refl equivalence f
+
+@[symm]
+theorem TuringEquivalent.symm {f g : ℕ →. ℕ} (h : f ≡ᵀ g) : g ≡ᵀ f :=
+  Equivalence.symm equivalence h
+
+@[trans]
+theorem TuringEquivalent.trans (f g h : ℕ →. ℕ) (h₁ : f ≡ᵀ g) (h₂ : g ≡ᵀ h) : f ≡ᵀ h :=
+  Equivalence.trans equivalence h₁ h₂
+
+/--
+Instance declaring that `RecursiveIn` is a preorder.
+-/
+instance : IsPreorder (ℕ →. ℕ) TuringReducible where
+  refl := TuringReducible.refl
+  trans := @TuringReducible.trans
+
+lemma partrec_iff_partrec_in_empty (f : ℕ →. ℕ) : Nat.Partrec f ↔ RecursiveIn {} f := by
+  constructor
+  intros pF
+  · induction' pF with f' g' _ _ ih₁ ih₂ f' g' _ _ ih₁ ih₂ f' g' _ _ ih₁ ih₂ f' _ ih
+    repeat {constructor}
+    · case pair =>
+      apply RecursiveIn.pair ih₁ ih₂
+    · case comp =>
+      apply RecursiveIn.comp ih₁ ih₂
+    · case prec =>
+      apply RecursiveIn.prec ih₁ ih₂
+    · case rfind =>
+      apply RecursiveIn.rfind ih
+  · intro fRecInNone
+    induction' fRecInNone with g hg g h _ _ ih₁ ih₂ g h _ _ ih₁ ih₂ g h _ _ ih₁ ih₂ g _ ih
+    repeat {constructor}
+    · simp at hg
+    repeat {constructor; assumption; try assumption}
+
+/--
+An alternative definition of partial recursive in terms of oracle computability:
+A partial recursive function is a function which is recursive in the empty set
+-/
+def Partrec₃ (f : ℕ →. ℕ) : Prop :=
+  RecursiveIn {} f
