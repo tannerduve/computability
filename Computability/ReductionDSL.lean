5d2459bee3168631ade804d69c507ed19427c711
tannerduve <tannerduve@gmail.com>
2025-05-16 16:23:24 -0400

    Comment

--- Computability/ReductionDSL.lean
+++ Computability/ReductionDSL.lean
@@ -1,1 +1,6 @@
 import Computability.TuringDegree
+
+/-
+Would be great to write a DSL for writing reductions that compiles into `RecursiveIn` proofs.
+Can brainstorm/start implementing it here.
+-/
