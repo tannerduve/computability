2c9c23a186cbc81072ba24691da32e6815d3c4c5
tannerduve <tannerduve@gmail.com>
2025-05-21 01:10:15 -0400

    fix

--- Computability/ArithHierarchy.lean
+++ Computability/ArithHierarchy.lean
@@ -48,35 +48,3 @@ def Delta0 (n : ℕ) (A : Set ℕ) : Prop :=
 notation "Σ⁰_" => Sigma0
 notation "Π⁰_" => Pi0
 notation "Δ⁰_" => Delta0
-
-lemma Sigma0_mono {m n : ℕ} (h : m ≤ n) (A : Set ℕ) : Sigma0 m A → Sigma0 n A := by
-  induction' n with n ih generalizing m
-  · intro hA
-    simp [Sigma0, decidableIn] at hA ⊢
-    simp at h
-    simp [h] at hA
-    cases' hA with f hf
-    use f
-  · intro hA
-    simp [Sigma0, recursively_enumerable_in] at hA ⊢
-    induction' m with m ih₁
-    · simp at hA
-      simp [decidableIn] at hA
-      cases' hA with f hf
-      let f' : ℕ →. ℕ := λ x => if f x then Part.some x else Part.none
-      use f'
-      constructor
-      · simp [ComputableIn] at hf
-        cases' hf with lt hf'
-        simp [f']
-        specialize @ih 0 (by linarith)
-        simp [Sigma0, decidableIn, ComputableIn] at ih
-        specialize ih f lt hf'
-        sorry
-      · sorry
-    · have : m ≤ n + 1 := by linarith
-      specialize ih₁ this
-      simp at hA
-      cases' hA with f hf
-      use f
-      sorry
